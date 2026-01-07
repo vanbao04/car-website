20cefa7d1003c3e92ba9ebe1fcfd17a68e36bdab
vanbao04 <145584111+vanbao04@users.noreply.github.com>
2026-01-07 21:46:25 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -4,35 +4,13 @@ const cars = [
     name: "Toyota Camry 2023",
     price: 1200000000,
     brand: "Toyota",
-    image: "https://via.placeholder.com/300",
+    image: "./images/camry.jpg"
   },
   {
     id: 2,
     name: "BMW X5",
     price: 3500000000,
     brand: "BMW",
-    image: "https://via.placeholder.com/300",
+    image: "./images/bmw.jpg"
   }
 ];
-
-const carList = document.getElementById("car-list");
-
-cars.forEach(car => {
-  const div = document.createElement("div");
-  div.className = "car-item";
-
-  div.innerHTML = `
-    <img src="${car.image}">
-    <h3>${car.name}</h3>
-    <p>Hãng: ${car.brand}</p>
-    <p>Giá: ${car.price.toLocaleString()} VNĐ</p>
-    <button onclick="viewDetail(${car.id})">Xem chi tiết</button>
-  `;
-
-  carList.appendChild(div);
-});
-
-function viewDetail(id) {
-  window.location.href = `detail.html?id=${id}`;
-}
-
