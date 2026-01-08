e73f6a17456790ddd23de49a6ed30154e3518e8b
vanbao04 <145584111+vanbao04@users.noreply.github.com>
2026-01-09 01:06:14 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -6,16 +6,36 @@ const cars = [
     type: "Supercar",
     price: 13500000000,
     image: "images/lamborghini.jpg",
-    tag: "HOT"
+    tag: "HOT",
+
+    engine: "V12 6.5L",
+    power: "770 HP",
+    acceleration: "2.8s (0-100 km/h)",
+    topSpeed: "350 km/h",
+    drivetrain: "AWD",
+    year: 2023,
+    origin: "Italy",
+    description: "Siêu xe V12 mạnh mẽ, giới hạn sản xuất, dành cho giới thượng lưu."
   },
+  
   {
     id: 2,
     name: "Ferrari LaFerrari",
     brand: "Ferrari",
     type: "Hypercar",
     price: 32000000000,
-    image: "images/ferrari1.jpg"
+    image: "images/ferrari1.jpg",
+
+    engine: "V12 Hybrid",
+    power: "963 HP",
+    acceleration: "2.6s (0-100 km/h)",
+    topSpeed: ">350 km/h",
+    drivetrain: "RWD",
+    year: 2022,
+    origin: "Italy",
+    description: "Hypercar hybrid biểu tượng của Ferrari, kết hợp sức mạnh và công nghệ tương lai."
   },
+  
   {
     id: 3,
     name: "Bugatti Chiron",
@@ -23,8 +43,18 @@ const cars = [
     type: "Megacar",
     price: 130000000000,
     image: "images/bugatti.jpg",
-    tag: "NEW"
+    tag: "NEW",
+
+    engine: "W16 Quad-Turbo",
+    power: "1500 HP",
+    acceleration: "2.4s (0-100 km/h)",
+    topSpeed: "420 km/h",
+    drivetrain: "AWD",
+    year: 2023,
+    origin: "France",
+    description: "Megacar nhanh nhất thế giới, đỉnh cao kỹ thuật và hiệu suất."
   },
+  
   {
     id: 4,
     name: "Koenigsegg One:1",
@@ -32,7 +62,16 @@ const cars = [
     type: "Megacar",
     price: 78000000000,
     image: "images/koenigsegg.jpg",
-    tag: "HOT"
+    tag: "HOT",
+    
+    engine: "V8 Twin-Turbo",
+    power: "1341 HP",
+    acceleration: "2.5s (0-100 km/h)",
+    topSpeed: ">400 km/h",
+    drivetrain: "RWD",
+    year: 2022,
+    origin: "Sweden",
+    description: "Siêu xe với tỷ lệ 1 mã lực trên 1 kg – cỗ máy tốc độ thuần túy."
   },
   {
     id: 5,
@@ -40,7 +79,16 @@ const cars = [
     brand: "KIA",
     type: "Hatchback",
     price: 380000000,
-    image: "images/morning.jpg"
+    image: "images/morning.jpg",
+
+    engine: "1.0L",
+    power: "67 HP",
+    acceleration: "14s (0-100 km/h)",
+    topSpeed: "150 km/h",
+    drivetrain: "FWD",
+    year: 2023,
+    origin: "Korea",
+    description: "Xe đô thị nhỏ gọn, tiết kiệm nhiên liệu, phù hợp đi lại hàng ngày."
   }
 ];
 
