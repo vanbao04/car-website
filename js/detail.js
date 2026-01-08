1a35fd119e911445f696361759bbd027c045e6d6
vanbao04 <145584111+vanbao04@users.noreply.github.com>
2026-01-09 00:38:10 +0700

    Update detail.js

--- js/detail.js
+++ js/detail.js
@@ -1,3 +1,48 @@
+const cars = [
+  {
+    id: 1,
+    name: "Lamborghini Aventador SVJ",
+    brand: "Lamborghini",
+    type: "Supercar",
+    price: 13500000000,
+    image: "images/lamborghini.jpg",
+    tag: "HOT"
+  },
+  {
+    id: 2,
+    name: "Ferrari LaFerrari",
+    brand: "Ferrari",
+    type: "Hypercar",
+    price: 32000000000,
+    image: "images/ferrari1.jpg"
+  },
+  {
+    id: 3,
+    name: "Bugatti Chiron",
+    brand: "Bugatti",
+    type: "Megacar",
+    price: 130000000000,
+    image: "images/bugatti.jpg",
+    tag: "NEW"
+  },
+  {
+    id: 4,
+    name: "Koenigsegg One:1",
+    brand: "Koenigsegg",
+    type: "Megacar",
+    price: 78000000000,
+    image: "images/koenigsegg.jpg",
+    tag: "HOT"
+  },
+  {
+    id: 5,
+    name: "KIA Morning",
+    brand: "KIA",
+    type: "Hatchback",
+    price: 380000000,
+    image: "images/morning.jpg"
+  }
+];
 
 const params = new URLSearchParams(window.location.search);
 const id = Number(params.get("id"));
@@ -15,6 +60,8 @@ if (car) {
     tagEl.innerText = car.tag;
     tagEl.style.display = "inline-block";
     tagEl.style.background = car.tag === "HOT" ? "#dc2626" : "#16a34a";
+  } else {
+    tagEl.style.display = "none";
   }
 }
 
