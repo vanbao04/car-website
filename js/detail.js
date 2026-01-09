73d7389d54e5190418a94fcf42eeacad552b68d8
vanbao04 <145584111+vanbao04@users.noreply.github.com>
2026-01-09 23:57:51 +0700

    Update detail.js

--- js/detail.js
+++ js/detail.js
@@ -1,24 +1,27 @@
-const params = new URLSearchParams(location.search);
+const params = new URLSearchParams(window.location.search);
 const id = Number(params.get("id"));
 
 const car = cars.find(c => c.id === id);
 
 if (!car) {
   alert("Không tìm thấy xe");
-  location.href = "index.html";
+  window.location.href = "index.html";
 }
 
 document.getElementById("car-image").src = car.image;
 document.getElementById("car-name").innerText = car.name;
-document.getElementById("car-brand").innerText = car.brand;
-document.getElementById("car-price").innerText =
-  car.price.toLocaleString() + " VND";
+document.getElementById("car-brand").innerText = "Hãng: " + car.brand;
+document.getElementById("car-year").innerText = "Năm: " + car.year;
+document.getElementById("car-price").innerText = car.price.toLocaleString() + " VND";
 
 document.getElementById("engine").innerText = car.engine;
 document.getElementById("power").innerText = car.power;
 document.getElementById("acceleration").innerText = car.acceleration;
 document.getElementById("topSpeed").innerText = car.topSpeed;
 document.getElementById("drivetrain").innerText = car.drivetrain;
-document.getElementById("year").innerText = car.year;
 document.getElementById("origin").innerText = car.origin;
 document.getElementById("description").innerText = car.description;
+
+function orderCar() {
+  alert("🚗 Đặt xe thành công! Showroom sẽ liên hệ bạn.");
+}
